3683235b67f9b2b3639aa540c00315c467500714
Eric Rainey <rainey75@me.com>
2011-07-02 22:19:49 -0400

    Callback Update
    Updated jquery.gplusone.js to render <g:plusone> tag & callbacks now working

--- jquery.gplusone.js
+++ jquery.gplusone.js
@@ -9,24 +9,19 @@
 (function($) {
 	$.fn.gPlusOne = function( options ) {
 		// if Google API Object does not exist, create script tag and call from Google server
-		if ( typeof gapi === "undefined") {
 			var gScript = document.createElement("script");
 			gScript.src = "https://apis.google.com/js/plusone.js";
 			$(gScript).appendTo('body');
-			}
 		// Settings for +1 Button	
-		var plusOneSettings = { href: window.location, size: "standard", count: "true", callback: false};
+		var plusOneSettings = { href: window.location, size: "standard", count: "true", callback: ""};
 		// Default options override
 		if ( options ) {
 			$.extend( plusOneSettings, options )
 				}
-		// Callback Option Check - need to revisit 6/2/2011
-		if(plusOneSettings.callback != false) {
-    		//$(plusOneSettings.callback);
-      		}
-		// Run Google API 'gapi.plusone.render' on target DOM element(s)
+        var plusOneButton = '<g:plusone href="' + plusOneSettings.href + '" size="' + plusOneSettings.size + '" count="' + plusOneSettings.size + '" callback="' + plusOneSettings.callback + '"></g:plusone>';
+		// Render +1 Button on target DOM element(s)
 		return this.each(function() { 
-			gapi.plusone.render(this, plusOneSettings);	
+			$(this).html( plusOneButton );	
 			})
 		}
 	});
