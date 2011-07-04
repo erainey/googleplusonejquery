f50d660229a883b2e468b348285e12afee7d2d94
Eric Rainey <rainey75@me.com>
2011-07-03 23:43:10 -0400

    Updated comments in script

--- jquery.gplusone.js
+++ jquery.gplusone.js
@@ -8,16 +8,16 @@
 
 (function($) {
 	$.fn.gPlusOne = function( options ) {
-		// if Google API Object does not exist, create script tag and call from Google server
-			var gScript = document.createElement("script");
-			gScript.src = "https://apis.google.com/js/plusone.js";
-			$(gScript).appendTo('body');
+		// create script tag and call Plus One script from Google
+		var gScript = document.createElement("script");
+		gScript.src = "https://apis.google.com/js/plusone.js";
+		$(gScript).appendTo('body');
 		// Settings for +1 Button	
 		var plusOneSettings = { href: window.location, size: "standard", count: "true", callback: ""};
 		// Default options override
 		if ( options ) {
 			$.extend( plusOneSettings, options )
-				}
+			}
         var plusOneButton = '<g:plusone href="' + plusOneSettings.href + '" size="' + plusOneSettings.size + '" count="' + plusOneSettings.size + '" callback="' + plusOneSettings.callback + '"></g:plusone>';
 		// Render +1 Button on target DOM element(s)
 		return this.each(function() { 
